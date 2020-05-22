4121441bcf467b086a0acb6cf8eff5d51266be66
Umar Saidu Auna <umarmanofpeace@gmail.com>
2020-05-22 11:21:26 +0100

    feat: 🎸 Quicksort Algorithm

--- src/com/algorithm/tower_of_hanoi/TowerofHanoi.java
+++ src/com/algorithm/tower_of_hanoi/TowerofHanoi.java
@@ -4,7 +4,7 @@ import com.algorithm.Print;
 
 public class TowerofHanoi {
     public static void main(String[]args){
-        Hanoi hanoi = new Hanoi(11);
+        Hanoi hanoi = new Hanoi(3);
     }
 
 
